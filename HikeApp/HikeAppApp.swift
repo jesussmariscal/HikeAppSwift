ee8f28954df0af51136049e9f5aa86c23cc90e9f
jesussmariscal <mariscalalonso16@icloud.com>
2024-01-07 21:01:05 +0100

    Initial Commit

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Jesus Mariscal on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
